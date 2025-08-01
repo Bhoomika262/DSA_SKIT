4ae26b28fdafa88690e9e003a2ce0a3756040bc3
Bhoomika <bhoomika.jalandhra@gmail.com>
2025-08-01 10:51:14 +0530

    Added DSA program to enter and display an array of 4 elements

---
+++ array_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[4],i,n;
+    for(i=0;i<4;i++)
+    {
+        cout<<"Enter the array["<<i<<"]";
+        cin>>a[i];
+    }
+    for(i=0;i<4;i++)
+    {
+        cout<<a[i]<<"\t";
+    }
+}
