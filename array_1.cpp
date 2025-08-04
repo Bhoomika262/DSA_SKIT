7ff335bf589c481c2200442c5d640b88a864ee4a
Bhoomika <bhoomika.jalandhra@gmail.com>
2025-08-04 12:17:57 +0530

    Add DSA program to enter and display an array of 5 elements

--- array_1.cpp
+++ array_1.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main()
 {
     int a[4],i;
-    for(i=0;i<4;i++)
+    for(i=0;i<=4;i++)
     {
         cout<<"Enter the array["<<i<<"]: ";
         cin>>a[i];
     }
-    for(i=0;i<4;i++)
+    for(i=0;i<=4;i++)
     {
         cout<<a[i]<<"\t";
     }
